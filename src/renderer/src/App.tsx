ed05e0d89bb383a6d963ff8de887eb9267ed541c
Abel Gianmarco Llontop Meza <a.llontopmeza@gmail.com>
2025-01-31 03:25:42 -0500

    fix: corregir el enrutado en produccion cambiarlo por hashRouter

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
+import { HashRouter, Route, Routes } from 'react-router';
 import CredentialsPage from './components/pages/CredentialsPage';
 import CredencialTemporalPage from './components/pages/CredentialTemporal/CredencialTemporal';
 import HomePage from './components/pages/Home/HomePage';
@@ -7,13 +7,13 @@ import HomePage from './components/pages/Home/HomePage';
 
 function App(): JSX.Element {
   return (
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/temporal' element={<CredencialTemporalPage />} />
-          <Route path='/credentials' element={<CredentialsPage />} />
-        </Routes>
-      </BrowserRouter>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/temporal' element={<CredencialTemporalPage />} />
+        <Route path='/credentials' element={<CredentialsPage />} />
+      </Routes>
+    </HashRouter>
 
   )
 }
